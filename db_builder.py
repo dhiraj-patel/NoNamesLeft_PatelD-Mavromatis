a7178b8d83a3dda95aca4b8552f439cbfbf4a43c
VannaM <vmavromatis18@gmail.com>
2017-02-05 17:12:12 -0500

    edits

--- db_builder.py
+++ db_builder.py
@@ -1,14 +1,44 @@
 from pymongo import MongoClient
 import csv
 
-c = MongoClient("lisa.stuy.edu")
-ourDB = c.NoNamesLeft
-
 #-------- CSV stuff --------
+
 #open csv
 fObj = open("peeps.csv")
 #read csv
 d=csv.DictReader(fObj)
 
+obj2 = open('courses.csv')
+temp = csv.DictReader(obj2)
+
+#bc DictReader can only be iterated over once
+d2 = []
+for j in temp:
+    d2.append(j)
+
+#-------- make list (dic) to be inserted --------
+
+dic = []
+
 for i in d:
-    ourDB.foo.insert_one(i)
+    combine = {}
+    for j in i:
+        combine[j] = i[j]
+    for j in d2:
+        if (combine['id'] == j['id']):
+            combine[j['code']] = j['mark']
+    dic.append(combine)
+
+fObj.close()
+obj2.close()
+
+
+#-------- mongo stuf --------
+
+c = MongoClient('149.89.150.102')
+ourDB = c.NoNamesLeft
+
+for i in dic:
+    #print type(i)
+    ourDB.students.insert_one(i)
+#ourDB.students.insert_many(dic)
