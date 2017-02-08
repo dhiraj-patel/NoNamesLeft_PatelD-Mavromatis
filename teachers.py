791fa7edcc4d95d2bbb50cea72d573df435d1695
VannaM <vmavromatis18@gmail.com>
2017-02-07 19:03:48 -0500

    6.75

--- teachers.py
+++ teachers.py
@@ -27,7 +27,7 @@ for i in d:
     studentList = ourDB.students.find({key: {"$exists": True}})
 
     for j in studentList:
-        teacher['students'].append(j['name'][0])
+        teacher['students'].append(j['name'][1])
 
     print teacher
     #ourDB.teachers.insert_one(teacher)
